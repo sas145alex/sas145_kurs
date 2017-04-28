92ed806b705bf3aaa95398ea52b00a21c72a0584
Alex Sazonov <sas145alex@gmail.com>
2017-04-28 20:05:05 +0300

    Доделал мультиселекты у Автора и Книги. Перехожу к созданию сущности Расположение.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -69,6 +69,6 @@ class AuthorsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def author_params
-      params.require(:author).permit(:fn, :ln, :sn, :author_index)
+      params.require(:author).permit(:fn, :ln, :sn, :author_index, book_ids: [])
     end
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -4,4 +4,8 @@ class Author < ActiveRecord::Base
   validates :author_index, :fn, :ln, presence: true
   validates :author_index, length: { minimum: 2 }, allow_blank: true
   validates :author_index, uniqueness: { scope: :ln, message: "уже есть у одного из авторов-софамильцев"}
+
+  def name_with_initial
+    "#{fn.first}. #{ln}"
+  end
 end
